f6335603c0ece007e92c4d74b0e4f7e95efa923d
Wayne Pretorius <67366859+WaynePretorius@users.noreply.github.com>
2022-12-20 13:20:30 +0200

    Initial Push

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,8 @@
+/// <summary>
+/// Tags is the module that I use in order to keep track of all string references.  Makes debugging 100x easier
+/// </summary>
+public static class Tags 
+{
+    //Audio
+    private static string MIXER_MASTER_VOLUME = "mixerMasterVolume";
+}
